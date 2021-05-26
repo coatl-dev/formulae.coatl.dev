88f4a2d78fa4da422ce3b8c2cc70bc8a51b67c65
César Román <thecesrom@gmail.com>
2021-05-25 23:05:58 -0700

    docs(homebrew): update referenced formulae

--- script/generate-core.rb
+++ script/generate-core.rb
@@ -6,7 +6,7 @@ tap_name = "Homebrew/core"
 tap = Tap.fetch(tap_name)
 
 directories = ["_data/#{formula_dir}", "api/#{formula_dir}", "#{formula_dir}"]
-# FileUtils.rm_rf directories + ["_data/#{formula_dir}_canonical.json"]
+FileUtils.rm_rf directories + ["_data/#{formula_dir}_canonical.json"]
 FileUtils.mkdir_p directories
 
 json_template = IO.read "_api_formula.json.in"
