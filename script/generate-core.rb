445798b2520b34e074eb8f323821b30f813871d1
César Román <thecesrom@gmail.com>
2021-07-09 12:32:35 -0700

    refactor(script): :truck: relocate formula and bottle data for Homebrew/core formulae

--- script/generate-core.rb
+++ script/generate-core.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env brew ruby
+os = "mac"
 formula_dir = "formula-core"
 bottle_dir = "bottle"
 tap_name = "Homebrew/core"
 
-# formula_dir = os == "mac" ? "formula" : "formula-linux"
+formula_dir = os == "mac" ? "formula-core" : "formula-core-linux"
+bottle_dir = os == "mac" ? "bottle-core" : "bottle-core-linux"
 tap = Tap.fetch(tap_name)
 
 directories = ["_data/#{formula_dir}", "_data/#{bottle_dir}", "api/#{formula_dir}", "api/#{bottle_dir}", "#{formula_dir}"]
@@ -29,8 +31,8 @@ tap.formula_names.each do |n|
     IO.write("_data/#{formula_dir}/#{f.name.tr("+", "_")}.json", "#{JSON.pretty_generate(f.to_hash)}\n")
     IO.write("_data/#{bottle_dir}/#{f.name.tr("+", "_")}.json", "#{JSON.pretty_generate(f.to_recursive_bottle_hash)}\n")
     IO.write("api/#{formula_dir}/#{f.name}.json", json_template)
-    IO.write("#{formula_dir}/#{f.name}.html", html_template.gsub("title: $TITLE", "title: \"#{f.name}\""))
     IO.write("api/#{bottle_dir}/#{f.name}.json", bottle_template)
+    IO.write("#{formula_dir}/#{f.name}.html", html_template.gsub("title: $TITLE", "title: \"#{f.name}\""))
   end
 end
 IO.write("_data/#{formula_dir}_canonical.json", "#{JSON.pretty_generate(tap.formula_renames.merge(tap.alias_table))}\n")
