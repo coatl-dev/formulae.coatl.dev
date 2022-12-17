628568730f0910ac1bc3a3c4e7bc43f5c6f2395b
César Román <thecesrom@gmail.com>
2022-12-16 17:43:39 -0800

    chore: update core formulae

--- script/generate-core.rb
+++ script/generate-core.rb
@@ -17,18 +17,36 @@ bottle_template = IO.read "_api_bottle.json.in"
 html_template = IO.read "_formula.html.in"
 
 tap.formula_names.each do |n|
-  if ["autoconf",
+  if ["alsa-lib",
+      "autoconf",
       "autoconf@2.13",
       "autoconf@2.69",
+      "bzip2",
+      "cups",
+      "fontconfig",
       "freetype",
       "freetype2",
+      "giflib",
+      "harfbuzz",
+      "jpeg-turbo",
       "libpng",
+      "libx11",
+      "libxext",
+      "libxrandr",
+      "libxrender",
+      "libxt",
+      "libxtst",
+      "little-cms2",
       "m4",
       "openjdk",
       "openjdk@8",
       "openjdk@11",
       "openjdk@17",
-      "pkg-config"].include?(n)
+      "perl",
+      "pkg-config",
+      "unzip",
+      "zlib",
+      "zip"].include?(n)
     f = Formulary.factory(n)
     IO.write("_data/#{formula_dir}/#{f.name.tr("+", "_")}.json", "#{JSON.pretty_generate(f.to_hash)}\n")
     IO.write("_data/#{bottle_dir}/#{f.name.tr("+", "_")}.json", "#{JSON.pretty_generate(f.to_recursive_bottle_hash)}\n")
