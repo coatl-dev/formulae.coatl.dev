dbd9b45f21c776dd71fd1f1581a9176a9548df86
César Román <cesar@coatl.dev>
2023-10-17 08:18:34 -0700

    ci(generate): create script file

---
+++ script/find-replace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+keys=(
+  "build_dependencies"
+  "test_dependencies"
+)
+for key in "${keys[@]}"
+do
+  find _data/formula -name '*.json' -type f -exec sed -i '' "s/$key/core_$key/g" {} +
+done
